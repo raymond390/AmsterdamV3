555ef49d6f063d3d6ef21963d56adec1c1e4aa99
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-27 21:25:28 +0100

    updatje

--- commands/info.js
+++ commands/info.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     var botEmbed = new discord.MessageEmbed()
             .setTitle('Informatie')
             .setColor("#0099ff")
-            .setDescription("Deze Bot is gemaakt bij **Raymond#1362**\n")
+            .setDescription("Deze Bot is gemaakt door **Raymond#1362**\n")
             .addFields(
               
             )
