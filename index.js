23cd2b5be93f9ebab1642427be40b3d5cc0aecb3
raymond390 <74500294+raymond390@users.noreply.github.com>
2020-12-27 21:17:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -105,9 +105,9 @@ client.on("guildMemberRemove", member => {
 
 
 const activities_list = [
-    "DSI V3", 
+    "Amsterdam V3", 
     ".help",
-    "DSI V3", 
+    "Amsterdam V3", 
     ".help"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
     
